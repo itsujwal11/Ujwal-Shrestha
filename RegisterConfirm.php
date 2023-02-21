546294e4a6ba292be3b055af9e222b759dd71a81
itsujwal11 <123341339+itsujwal11@users.noreply.github.com>
2023-02-21 20:22:07 +0545

    Create RegisterConfirm.php

---
+++ RegisterConfirm.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Congrats Box</title>
+    <link rel="stylesheet" href="registerconfirm.css">
+</head>
+<body>
+    <div class="container">
+        <div class="popup" id="popup">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/1024px-Flat_tick_icon.svg.png" width="110px" height="100px">
+            <br>
+            <h2>Thank You!</h2>
+           
+            <p>Your details has been successfully submitted. </p>
+            <br>
+               <p> Thanks!</p>
+            <button type="submit" class="btn"><a href="index.php">Submit</a></button>
+
+        </div>
+    </div>
+
+</body>
+</html>
